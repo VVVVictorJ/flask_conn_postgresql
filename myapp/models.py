2d87340ab9dd6a09dd4055fe772c10d451c8953d
VVVVictorJ <VictorRobin1097@gmail.com>
2020-03-31 12:15:35 +0800

    add new table

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,8 @@ from app import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    
+    __table_args__ = {'useexisting': True}
+
     id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(64),unique=True,index=True)
     username = db.Column(db.String(64),unique=True,index=True)
@@ -17,4 +18,18 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
-    
+
+class UserProfile(db.Model):
+    __tablename__ = 'userprofile'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, index = True)
+    userimage = db.Column(db.String(64))
+
+    def __init__(self, userName, userImage):
+        self.username = username
+        self.userimage = userimage
+    def __repr__(self):
+        return '<UserProfile %r>' % self.username
+
+
